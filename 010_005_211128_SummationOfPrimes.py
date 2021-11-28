e81ea62119716b1d457f3815489d6f8835f87780
Y. Grace Yu <y.grace.yu@outlook.com>
2021-11-28 21:45:55 +0800

    Add files via upload

---
+++ 010_005_211128_SummationOfPrimes.py
@@ -0,0 +1,22 @@
+
+
+print('''
+The sum of the primes below 10 is 2 + 3 + 5 + 7 = 17.
+Find the sum of all the primes below two million.
+
+Answer: 142913828922\n''')
+
+import math
+
+# n >= 2
+def prim(n):
+    for i in range(2,int(math.sqrt(n))+1):
+        if (n+.001)//i*i==n: return False
+    return True
+
+ans = 0
+for i in range(2,int(2e6)+1):
+    if prim(i): ans += i
+
+print(ans, '\n')
+
