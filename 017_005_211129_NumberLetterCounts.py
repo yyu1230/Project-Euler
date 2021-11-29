0ca95dffb7e022f0d9e78d36bfbba9238be901fc
Y. Grace Yu <y.grace.yu@outlook.com>
2021-11-29 11:32:17 +0800

    Update 017_005_211129_NumberLetterCounts.py

--- 017_005_211129_NumberLetterCounts.py
+++ 017_005_211129_NumberLetterCounts.py
@@ -6,7 +6,8 @@ then there are 3 + 3 + 5 + 4 + 4 = 19 letters used in total.
 If all the numbers from 1 to 1000 (one thousand) inclusive were written out in
 words, how many letters would be used?
 NOTE: Do not count spaces or hyphens. For example, 342 (three hundred and forty-
-two) contains 23 letters and 115 (one hundred and fifteen) contains 20 letters. The use of "and" when writing out numbers is in compliance with British usage.
+two) contains 23 letters and 115 (one hundred and fifteen) contains 20 letters.
+The use of "and" when writing out numbers is in compliance with British usage.
 
 Answer: xxxx\n''')
 
